9df65c577278d35209725fc4dd4ebe12a5da8a2c
billperegoy <bill@billperegoy.com>
2014-06-06 16:52:42 -0400

    Commit chnges that work before factoring out a Frame class.

--- lib/BowlingGame.rb
+++ lib/BowlingGame.rb
@@ -1,9 +1,13 @@
 class BowlingGame
+  attr_reader :score
+
   def initialize
     @score = 0
     @frame = 1
     @frame_score = 0
     @frame_roll = 0
+
+    @strike_add = 0
     @spare_add = false
   end
 
@@ -13,31 +17,45 @@ class BowlingGame
       @score += pins
       @spare_add = false
     end
-    @frame_score += pins
-    @frame_roll += 1
-
-    if strike
-    elsif spare
-      @spare_add = true
-    elsif frame_complete
+    if @strike_add > 0
+      @score += pins
+      @strike_add -= 1
     end
+    @frame_score += pins
+    @frame_roll += 1 
+
+    check_for_end_of_frame
   end
 
-
-  def score
-    @score
-  end
 
   private
-  def strike
+  def strike?
     (@frame_roll == 1) && (@frame_score == 10)
   end
 
-  def spare
+  def spare?
     (@frame_roll == 2) && (@frame_score == 10)
   end
 
-  def frame_complete
+  def frame_complete?
     @frame_roll == 2
+  end
+
+  def check_for_end_of_frame
+    if strike?
+      @strike_add += 2
+      finish_frame
+    elsif spare?
+      @spare_add = true
+      finish_frame
+    elsif frame_complete?
+      finish_frame
+    end
+  end
+
+  def finish_frame
+    @frame += 1
+    @frame_score = 0
+    @frame_roll = 0
   end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -3,28 +3,76 @@ require_relative '../lib/BowlingGame'
 describe BowlingGame do
   it "should return 0 for a 10 zero rolls" do
     g = BowlingGame.new
-    (1..10).each { g.roll(0) }
+    (1..10).each do
+      2.times { g.roll(0) }
+    end
     g.score.should == 0
   end
 
-  it "should return 10 for a 10 1 pin rolls" do
+  it "should return 20 for a 20 1 pin rolls" do
     g = BowlingGame.new
-    (1..10).each { g.roll(1) }
-    g.score.should == 10
+    (1..10).each do
+      2.times { g.roll(1) } 
+    end
+    g.score.should == 20
   end
 
   it "should properly score a first frame spare" do
     g = BowlingGame.new
-    g.roll(5)
-    g.roll(5)
-    (1..9).each { g.roll(1) }
-    g.score.should == 20
+    2.times { g.roll(5) }
+    (2..10).each do 
+      2.times { g.roll(1) }
+    end
+    g.score.should == 29
   end
 
-  #it "should properly score a first frame strike" do
-  #  g = BowlingGame.new
-  #  g.roll(10)
-  #  (1..9).each { g.roll(1) }
-  #  g.score.should == 21
-  #end
+  it "should properly score two spares in a row" do
+    g = BowlingGame.new
+    2.times { g.roll(5) }
+    2.times { g.roll(5) }
+    (3..10).each do 
+      2.times { g.roll(1) }
+    end
+    g.score.should == (15 + 11 + 16)
+  end
+
+  it "should properly score a first frame strike" do
+    g = BowlingGame.new
+    g.roll(10)
+    (2..10).each do
+      2.times { g.roll(1) }
+    end
+    g.score.should == (12 + 18)
+  end
+
+  it "should properly score two strikes in a row" do
+    g = BowlingGame.new
+    g.roll(10)
+    g.roll(10)
+    (3..10).each do
+      2.times { g.roll(1) }
+    end
+    g.score.should == (21 + 12 + 16)
+  end
+
+  it "should properly score three strikes in a row" do
+    pending "Doesn't work yet"
+    g = BowlingGame.new
+    g.roll(10)
+    g.roll(10)
+    g.roll(10)
+    (4..10).each do
+      2.times { g.roll(1) }
+    end
+    g.score.should == (30 + 21 + 12 + 14)
+  end
+
+  it "should properly score a perfect game" do
+    pending "Doesn't work yet"
+    g = BowlingGame.new
+    (1..10).each do
+      g.roll(10) 
+    end
+    g.score.should == 300 
+  end
 end
