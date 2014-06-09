c7efbeefe1bca7e5fa1328bc851b1072ce81e084
billperegoy <bill@billperegoy.com>
2014-06-08 21:02:48 -0400

    Took away diredct access to @roll in Frame. Changed @real method name to make it clearer. Cleaned up long conditional in BowlingGame to make the logic clearer.

--- lib/BowlingGame.rb
+++ lib/BowlingGame.rb
@@ -2,9 +2,8 @@
 # Implements two main public methids:
 #   - score
 #   - roll(pins)
-##
+#
 class BowlingGame
-
   def initialize
     @frames = (0..12).map { |num|  Frame.new(num) }
     @frame_number = 1
@@ -25,7 +24,7 @@ class BowlingGame
 
   private
   def add_spare_bonus(pins)
-    if previous_frame.spare? && first_roll
+    if previous_frame.spare? && current_frame.first_roll?
       previous_frame.add_to_bonus_score(pins)
     end
   end
@@ -42,21 +41,17 @@ class BowlingGame
   end
 
   def add_strike_bonus_to_two_frames_ago(pins)
-    if previous_frame.strike? && two_frames_ago.strike? && first_roll
+    if strikes_in_past_two_frames? && current_frame.first_roll?
       two_frames_ago.add_to_bonus_score(pins)
     end
   end
 
+  def strikes_in_past_two_frames?
+     previous_frame.strike? && two_frames_ago.strike?
+  end
+
   def check_for_end_of_frame
     @frame_number += 1  if current_frame.complete?
-  end
-
-  def first_roll
-    current_frame.roll == 1
-  end
-
-  def second_roll
-    current_frame.roll == 2
   end
 
   def current_frame
